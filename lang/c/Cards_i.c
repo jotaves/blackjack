311304c18ec3ab82eddc8e4fe75586d61cc739f4
João Victor <victorbarboza@outlook.com>
2019-06-16 19:36:52 -0300

    Starting Dealer

--- lang/c/Cards_i.c
+++ lang/c/Cards_i.c
@@ -6,9 +6,11 @@
 /* Basic constants */
 
 /* Array and record constants */
+
+const int32_t Cards__cards_points_i[13] = {11,2,3,4,5,6,7,8,9,10,10,10,10};
 /* Clause CONCRETE_VARIABLES */
 
-static int32_t Cards__cards_points_i[52];
+static bool Cards__deck_cards_i[52];
 /* Clause INITIALISATION */
 void Cards__INITIALISATION(void)
 {
@@ -17,96 +19,9 @@ void Cards__INITIALISATION(void)
         int32_t ii;
         
         ii = 0;
-        while((ii) <= (51))
+        while(((ii) <= (51)))
         {
-            if((ii) <= (3))
-            {
-                Cards__cards_points_i[ii] = 11;
-            }
-            else
-            {
-                if((ii) <= (7))
-                {
-                    Cards__cards_points_i[ii] = 2;
-                }
-                else
-                {
-                    if((ii) <= (11))
-                    {
-                        Cards__cards_points_i[ii] = 3;
-                    }
-                    else
-                    {
-                        if((ii) <= (15))
-                        {
-                            Cards__cards_points_i[ii] = 4;
-                        }
-                        else
-                        {
-                            if((ii) <= (19))
-                            {
-                                Cards__cards_points_i[ii] = 5;
-                            }
-                            else
-                            {
-                                if((ii) <= (23))
-                                {
-                                    Cards__cards_points_i[ii] = 6;
-                                }
-                                else
-                                {
-                                    if((ii) <= (27))
-                                    {
-                                        Cards__cards_points_i[ii] = 7;
-                                    }
-                                    else
-                                    {
-                                        if((ii) <= (31))
-                                        {
-                                            Cards__cards_points_i[ii] = 8;
-                                        }
-                                        else
-                                        {
-                                            if((ii) <= (35))
-                                            {
-                                                Cards__cards_points_i[ii] = 9;
-                                            }
-                                            else
-                                            {
-                                                if((ii) <= (39))
-                                                {
-                                                    Cards__cards_points_i[ii] = 10;
-                                                }
-                                                else
-                                                {
-                                                    if((ii) <= (43))
-                                                    {
-                                                        Cards__cards_points_i[ii] = 10;
-                                                    }
-                                                    else
-                                                    {
-                                                        if((ii) <= (47))
-                                                        {
-                                                            Cards__cards_points_i[ii] = 10;
-                                                        }
-                                                        else
-                                                        {
-                                                            if((ii) <= (51))
-                                                            {
-                                                                Cards__cards_points_i[ii] = 10;
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            Cards__deck_cards_i[ii] = true;
             ii = ii+1;
         }
     }
